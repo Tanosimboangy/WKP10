88bbf2bc07fb07ea024303f886e466fde7287101
Tanosimboangy <jacquit.aja@onja.org>
2020-09-12 16:43:49 +0300

    cleaning vs code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import faker from 'faker';
-
 const tbody = document.querySelector('tbody');
-
-let persons = Array.from({ length: 10 }, () => {
+function wait(ms = 0) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let persons = Array.from({ length: 4 }, () => {
 	return {
 		id: faker.random.uuid(),
 		lastName: faker.name.lastName(),
@@ -13,12 +14,11 @@ let persons = Array.from({ length: 10 }, () => {
 		picture: faker.image.avatar(100, 100),
 	};
 });
-
 const displayList = data => {
 	tbody.innerHTML = data
-		.map(
-			(person, index) => `
-    <tr data-id="${person.id}" class="${index % 2 ? 'even' : ''}">
+	.map(
+		(person, index) => 
+	`<tr data-id="${person.id}" class="${index % 2 ? 'even' : ''} container">
         <td><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></td>
         <td class="last_name">${person.lastName}</td>
         <td class="first_name">${person.firstName}</td>
@@ -26,71 +26,80 @@ const displayList = data => {
         <td class="job_area">${person.jobArea}</td>
         <td class="phone">${person.phone}</td>
         <td>
-            <button class="edit">
+            <button class="edit" data-id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
             </button>
-            <button class="delete">
+            <button class="delete" data-id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
             </button>
         </td>
     </tr>`).join('');
 };
+const editPartner = (e) => {
+	const editButton = e.target.closest(".edit");
+	if (editButton) {
+		const tableRow = e.target.closest('.container');
+		const idToEdit = tableRow.dataset.id;
+		console.log(idToEdit);
+		 editPartnerPopup(idToEdit)
+	}	
+};
 
-
-const editPartner = e => {
-	// Grabbing the edit button with event delegation
-	const edit = e.target.closest(".edit");
-	console.log(edit);
-	if (edit) {
-		editPartnerPopup();
-	}
+async function destroyPopup(popup) {
+	popup.classList.remove('open');
+	popup.remove();
 }
 
-// Grabbing the tr elements in order to get the value inside
-	// const lastname = e.target.matches(".last_name");
-	// const firstname = e.target.matches(".first_name");
-	// const jobtitle = e.target.matches(".job_title");
-	// const jobarea = e.target.matches(".job_area");
-	// const phonenumber = e.target.matches(".phone");
-
-	// console.log(lastname, firstname, jobtitle, jobarea, phonenumber);
-
-// const cancel = e => {
-// 	// Activating the cancel button
-// 	const cancel = e.target.closest(".cancel");
-// 	if (cancel) {
-// 		destroyPopup();
-// 	}
-// 	console.log(cancel);
-// }
-// async function destroyPopup(popup) {
-//     popup.classList.remove("open");
-// }
-
-const editPartnerPopup = (e) => {
-	// create edit popup here
+const editPartnerPopup = idToEdit => {
+	const editpersons = persons.find(person => person.id === idToEdit);
+	console.log(editpersons);
+	return new Promise(async resolve => {
 	const popup = document.createElement('form');
 	popup.classList.add('popup');
-	// Insert the html inside of the popup form
 	popup.insertAdjacentHTML('afterbegin', 
 		`<fieldset>
 			<label>Last Name</label>
-			<input type="text" value="${faker.name.lastName()}">
+			<input type="text" name="lastName" value="${editpersons.lastName}">
 			<label>First Name</label>
-			<input type="text" value="${faker.name.firstName()}">
+			<input type="text" name="firstName" value="${editpersons.firstName}">
 			<label>Job Title</label>
-			<input type="text" value="${faker.name.jobTitle()}">
+			<input type="text" name="jobTitle" value="${editpersons.jobTitle}">
 			<label>Job Area</label>
-			<input type="text" value="${faker.name.jobArea()}">
+			<input type="text" name="jobArea" value="${editpersons.jobArea}">
 			<label>Phone Number</label>
-			<input type="text" value="${faker.phone.phoneNumber()}">
-			<button type="submit" class="save">Save</button>
-			<button type="submit" class="cancel">Cancel</button>
-		</fieldset>`);	
-	popup.classList.add('open');
-	// Append the popup inside of the html 
-	document.body.appendChild(popup);
+			<input type="text" name="phone" value="${editpersons.phone}">
+			<button class="submit" data-id="${editpersons.id}">Submit</button>
+		</fieldset>`);
+
+		const CancelButton = document.createElement('button');
+		CancelButton.type = 'button';
+		CancelButton.classList.add('cancel');
+		CancelButton.textContent = 'cancel';
+		popup.appendChild(CancelButton);
+		CancelButton.addEventListener('click', () => { resolve();
+				destroyPopup(popup);
+			},	{ once: true });
+
+		popup.addEventListener('submit', e => { 
+			e.preventDefault();
+			editpersons.firstName = popup.firstName.value;
+			editpersons.lastName = popup.lastName.value;
+			editpersons.jobTitle = popup.jobTitle.value;
+			editpersons.jobArea = popup.jobArea.value;
+			editpersons.phone = popup.phone.value;
+			displayList(persons);
+			destroyPopup(popup);
+		}, { once: true });
+		document.body.appendChild(popup);
+		popup.classList.add('open');
+	});
 };
+
+
+
+
+
+
 
 // code delete function here
 const deletePartner = e => {
@@ -98,68 +107,35 @@ const deletePartner = e => {
 	const remove = e.target.closest(".delete");
 	console.log(remove);
 	if (remove) {
-		deleteDeletePopup(remove);
+		const list = remove.closest('tr');
+		const id = list.dataset.id;
+		deleteDeletePopup(id);
 	}
 }
 
-const deleteDeletePopup = (e) => {
+const deleteDeletePopup = (id) => {
 	return new Promise(async function(resolve) {
-		// create confirmation popup here
+	// create confirmation popup here
 	const popup = document.createElement('form');
 	popup.classList.add('popup');
 	// Insert the html inside of the popup form
 	popup.insertAdjacentHTML('afterbegin', 
-		`<fieldset>
-			<h2>Are you sure you want to delete this?</h2>
-	</fieldset>`);	
-	popup.classList.add('open');
+			`<h2>Are you sure you want to delete this?</h2>`);	
+		const button = document.createElement("button");
+		button.textContent = "delete"
+		popup.appendChild(button);
+		popup.classList.add('open');
+		button.addEventListener("click", (e) => {
+			e.preventDefault();
+			persons = persons.filter(person => person.id !== id);
+			displayList(persons)
+			popup.classList.remove("open")
+		})
 	// Append the popup inside of the html 
 	document.body.appendChild(popup);
-	});	
+	});
 };
 
-
-// const deleteBtn = document.createElement('button');
-// 	deleteBtn.classList.add("remove");
-// 	deleteBtn.textContent = "Delete";
-// 	deleteBtn.type = "button";
-// 	popup.appendChild(deleteBtn);
-// 	deleteBtn.addEventListener('click', remove);
-// 	console.log(deleteBtn);
-
-// 	const cancelBtn = document.createElement('button');
-// 	cancelBtn.classList.add("cancel");
-// 	cancelBtn.textContent = "cancel";
-// 	cancelBtn.type = "button";
-// 	popup.appendChild(cancelBtn);
-// 	cancelBtn.addEventListener('click', reset);
-// 	console.log(cancelBtn);
-
-{/* <button type="submit" class="remove">Delete</button>
-<button type="submit" class="cancel">Cancel</button> */}
-
-displayList(persons);
+ displayList(persons);
 window.addEventListener('click', editPartner);
 window.addEventListener('click', deletePartner);
-
-
-
-// const cancelButton  = document.createElement("button");
-//     cancelButton.type = "button";
-//     cancelButton.textContent = "cancel";
-//     const yesButton  = document.createElement("button");
-//     yesButton.type = "button";
-//     yesButton.textContent = "yes";
-//     //append the buttons to the div popup
-//     deletePopup.appendChild(yesButton);
-//     deletePopup.appendChild(cancelButton);
-//     deletePopup.classList.add("open");
-//     //append the div popup to the document
-//     document.body.appendChild(deletePopup);
-//     yesButton.addEventListener("click", (e) => {
-//       //take who gets clicked
-//       const deletedPerson = tbody.querySelector(`[value="${person.id}"]`);
-//       // and remove him
-//       deletedPerson.remove();
-//       deletePopup.classList.remove("open");
-//     });
---
+++ test.js
@@ -0,0 +1,193 @@
+import faker from 'faker';
+import wait from 'waait';
+const tbody = document.querySelector('tbody');
+let persons = Array.from({ length: 10 }, () => {
+	return {
+		id: faker.random.uuid(),
+		lastName: faker.name.lastName(),
+		firstName: faker.name.firstName(),
+		jobTitle: faker.name.jobTitle(),
+		jobArea: faker.name.jobArea(),
+		phone: faker.phone.phoneNumber(),
+		picture: faker.image.avatar(100, 100),
+	};
+});
+
+console.log(persons);
+
+const displayList = data => {
+	tbody.innerHTML = data
+		.map(
+			(person, index) => `
+    <tr data-id="${person.id}" class="${index % 2 ? 'even' : ''}">
+        <td><img src="${person.picture}" alt="${person.firstName + ' ' + person.lastName}"/></td>
+        <td>${person.lastName}</td>
+        <td>${person.firstName}</td>
+        <td>${person.jobTitle}</td>
+        <td>${person.jobArea}</td>
+        <td>${person.phone}</td>
+        <td>
+            <button class="edit" data-id="${person.id}">
+                <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
+            </button>
+            <button class="delete" data-id="${person.id}">
+                <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+            </td>
+    </tr>`).join('');
+};
+
+const editPartner = async id => {
+	const person = persons.find(person => person.id === id);
+	const result = await editPartnerPopup(person);
+	if (result) {
+		displayList(persons);
+	}
+};
+
+// async function destroyPopup(popup) {
+// 	popup.classList.remove('open');
+// 	await wait(1000);
+// 	popup.remove();
+// 	popup = null;
+// }
+
+const editPartnerPopup = person => {
+	return new Promise(async resolve => {
+		const popup = document.createElement('form');
+		popup.classList.add('popup');
+		popup.insertAdjacentHTML(
+			'afterbegin',
+			`<fieldset>
+				<h3>Edit</h3>
+                <label>Lastname</label>
+				<input type="text" name="lastName" value="${person.lastName}"/>
+                <label>Firstname</label>
+				<input type="text" name="firstName" value="${person.firstName}"/>
+                <label>Job title</label>
+				<input type="text" name="jobTitle" value="${person.jobTitle}"/>
+                <label>Job area</label>
+				<input type="text" name="jobArea" value="${person.jobArea}"/>
+                <label>Phone number</label>
+				<input type="text" name="phone" value="${person.phone}"/>
+				<button type="submit">Submit</button>
+            </fieldset>`);
+		const skipButton = document.createElement('button');
+		skipButton.type = 'button';
+		skipButton.textContent = 'Cancel';
+		popup.firstElementChild.appendChild(skipButton);
+		skipButton.addEventListener('click', () => { resolve(null);
+				destroyPopup(popup);
+			},	{ once: true });
+		popup.addEventListener( 'submit', e => { e.preventDefault();
+				person.firstName = e.target.firstName.value;
+				person.lastName = e.target.lastName.value;
+				person.jobTitle = e.target.jobTitle.value;
+				person.jobArea = e.target.jobArea.value;
+				person.phone = e.target.phone.value;
+				resolve(person);
+				destroyPopup(popup);
+			}, { once: true });
+		document.body.appendChild(popup);
+		popup.classList.add('open');
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const deletePartner = async id => {
+// 	const person = persons.find(person => person.id === id);
+// 	const result = await deleteDeletePopup(person);
+// 	if (result) {
+// 		persons = persons.filter(person => person.id !== result.id);
+// 		displayList(persons);
+// 	}
+// };
+
+// const deleteDeletePopup = person => {
+// 	return new Promise(async resolve => {
+// 		// create the html form
+// 		const popup = document.createElement('form');
+// 		popup.classList.add('popup');
+// 		popup.insertAdjacentHTML(
+// 			'afterbegin',
+// 			`<fieldset>
+// 				<h3>Delete ${person.firstName} ${person.lastName}</h3>
+//                 <p>Are you sure you want to delete this person from the list?</p>
+// 				<button type="submit">Delete</button>
+//             </fieldset>
+// 		`
+// 		);
+
+// 		const skipButton = document.createElement('button');
+// 		skipButton.type = 'button'; // so it doesn't submit
+// 		skipButton.textContent = 'Cancel';
+// 		popup.firstElementChild.appendChild(skipButton);
+// 		skipButton.addEventListener(
+// 			'click',
+// 			() => {
+// 				resolve(null);
+// 				destroyPopup(popup);
+// 			},
+// 			{ once: true }
+// 		);
+
+// 		popup.addEventListener(
+// 			'submit',
+// 			e => {
+// 				e.preventDefault();
+// 				// popup.input.value;
+// 				resolve(person);
+// 				destroyPopup(popup);
+// 			},
+// 			{ once: true }
+// 		);
+
+// 		document.body.appendChild(popup);
+// 		await wait(50);
+// 		popup.classList.add('open');
+// 	});
+// };
+
+// const handleClick = e => {
+// 	const deleteButton = e.target.closest('button.delete');
+// 	const editButton = e.target.closest('button.edit');
+
+// 	if (editButton) {
+// 		const idToEdit = editButton.dataset.id;
+// 		editPartner(idToEdit);
+// 	}
+
+// 	if (deleteButton) {
+// 		const idToDelete = deleteButton.dataset.id;
+// 		deletePartner(idToDelete);
+// 	}
+// };
+
+// tbody.addEventListener('click', handleClick);
+
+// displayList(persons);
